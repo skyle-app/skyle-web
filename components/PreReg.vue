1b91317f985a059715644eaf0adbcd8109c5c6e2
SSKATE <kms0219kms@naver.com>
2021-12-04 12:36:44 +0900

    Create PreReg.vue

---
+++ components/PreReg.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="download" class="contact-us section">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 offset-lg-3">
+          <div class="section-heading wow fadeIn" data-wow-duration="1s" data-wow-delay="0.5s">
+            <h6>사전 예약</h6>
+            <h4 class="downcccc"><em>지금 사전예약 하고 Skyle의 베타테스터가 되세요.</h4>
+            <div class="line-dec"></div>
+          </div>
+        </div>
+        <div class="col-lg-12 wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.25s">
+          <form id="downloads" action="" method="post">
+           <div id="contactt22" class="row">
+              <div class="col-lg-7">
+                <div class="fill-form">
+                  <div class="row">
+                      <div class="info-post">
+                        <a class="badgee2" href="http://prereg.skyle.kr">사전예약 바로가기</a>
+                      </div>
+                  </div>
+                </div>
+              </div>
+           </div>
+          </form>
+  </div>
+</template>
+
+<style scoped>
+.row#contactt22,.row#contactt2 {display: flex; justify-content: center; align-items: center;}
+.downcccc {font-size: 28px;}
+img.badgee {height: 10vh;}
+a.badgee2 {display: block;}
+.row h1 {margin-top:50px;}
+
+@media (max-width: 992px) {
+  img.badgee {height: auto !important;}
+  a.badgee2 {display: none !important;}
+}
+
+.fill-form {
+  padding: 80px 60px 80px 60px;
+}
+</style>
