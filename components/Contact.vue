9f85651c638806800b53c6f71d1b25a8f7a4cef0
Your Name <you@example.com>
2021-12-04 07:07:43 +0000

    e

--- components/Contact.vue
+++ components/Contact.vue
@@ -29,9 +29,9 @@
                       <div class="info-post">
                         <div class="icon">
                           <img src="/assets/images/phone-icon.png" alt="">
-                          <a href="tel:01085910631">010-8591-0631</a>
+                          <a href="tel:01094649924">010-9464-9924</a>
                           <br>
-                          <a href="tel:01000000000">010-0000-0000</a> 
+                          <a href="tel:01085910631">010-8591-0631</a> 
                           <br/>
                         </div>
                       </div>
