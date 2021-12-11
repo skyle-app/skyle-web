5d94736bef29fb8bf67bcec8c1d14fda6a0fb29b
SSKATE <kms0219kms@naver.com>
2021-12-11 10:09:52 +0900

    Update Header.vue

--- components/Header.vue
+++ components/Header.vue
@@ -14,15 +14,15 @@
             <!-- ***** Logo End ***** -->
             <!-- ***** Menu Start ***** -->
             <ul class="nav">
-              <li class="scroll-to-section"><a href="#top">Home</a></li>
-              <li class="scroll-to-section"><a href="#about">About</a></li>
-              <!-- <li class="scroll-to-section"><a href="#services">Services</a></li> --> <!-- 추후 오픈될 컴포넌트임 -->
-              <!-- <li class="scroll-to-section"><a href="#portfolio">Projects</a></li>
-              <li class="scroll-to-section"><a href="#blog">Blog</a></li> -->
-              <li class="scroll-to-section"><a href="#contact">Contact</a></li> 
-              <!-- <li class="scroll-to-section"><div class="border-first-button"><a href="#contact">Free Quote</a></div></li> -->
-              <!-- <li class="scroll-to-section"><div class="border-first-button"><a href="#download">Downloads</a></div></li> -->
-              <li class="scroll-to-section"><div class="border-first-button"><a href="#download">사전 예약</a></div></li> 
+              <li class="scroll-to-section"><router-link to="/#top">Home</router-link></li>
+              <li class="scroll-to-section"><router-link to="/#about">About</router-link></li>
+              <!-- <li class="scroll-to-section"><router-link to="/#services">Services</router-link></li> --> <!-- 추후 오픈될 컴포넌트임 -->
+              <!-- <li class="scroll-to-section"><router-link to="/#portfolio">Projects</router-link></li>
+              <li class="scroll-to-section"><router-link to="/#blog">Blog</router-link></li> -->
+              <li class="scroll-to-section"><router-link to="/#contact">Contact</router-link></li> 
+              <!-- <li class="scroll-to-section"><div class="border-first-button"><router-link to="/#contact">Free Quote</router-link></div></li> -->
+              <!-- <li class="scroll-to-section"><div class="border-first-button"><router-link to="/#download">Downloads</router-link></div></li> -->
+              <li class="scroll-to-section"><div class="border-first-button"><router-link to="/#download">사전 예약</router-link></div></li> 
             </ul>        
             <a class='menu-trigger'>
                 <span>Menu</span>
